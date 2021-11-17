06f1a2ba25bfac2ba6dc48f0ae0ff28a1784ce80
Voisard Maëlle Pauline <mvoisard@INTRANET.EPFL.CH@INJ-IN-SC-24.intranet.epfl.ch>
2021-11-17 15:55:59 +0100

    TEST PULL

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int main(int argc, char * argv[]) {
+    double x = 3.3;
+    int x_arr = round(x);
+    printf("%d", x_arr);
+    return 0;
+}
