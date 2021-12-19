41a8d5fd695a03b67a209e5c83160db56237a4b7
Adèle Soulier <adele@tsf-460-wpa-7-233.epfl.ch>
2021-12-19 14:32:04 +0100

    commentaires clean et interface graphique updtade

--- interface_graphique.py
+++ interface_graphique.py
@@ -3,14 +3,10 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import numpy as np
-import matplotlib as mpl
 import matplotlib.animation as animation
-from matplotlib.colors import LogNorm
-import matplotlib.cbook as cbook
-
 
 #Ouverture des données:
-GPGP = np.genfromtxt("/Users/adele/Desktop/test de code/actualisationGPGPkimarche.csv", delimiter=',')
+GPGP = np.genfromtxt("/Users/adele/Desktop/projet_prog/3ans_final.csv", delimiter=',')
 GPGP_highlight=np.copy(GPGP)
 GPS_points = np.genfromtxt("/Users/adele/Desktop/test de code/gps_inputs.csv", delimiter=',')
 
@@ -31,9 +27,6 @@ orth = ccrs.PlateCarree(central_longitude= 180, globe=None)
 ax = plt.axes(projection=orth)
 ax.stock_img()
 
-for i in range(1,GPS_points.shape[0]):
-    plt.scatter(x=GPS_points[i,2], y=GPS_points[i,3],color="black",s=1, alpha=0.5, transform= ccrs.PlateCarree())
-   
 '''
 #ax.add_feature(cf.OCEAN)
 #ax.coastlines()
@@ -41,9 +34,17 @@ for i in range(1,GPS_points.shape[0]):
 #ax.add_feature(cf.BORDERS)
 '''
 
+#Ajout des noms des pays et du continent:
+ax.text(-152, 42, 'The GPGP', transform=ccrs.PlateCarree(), size=5)
+names=[['The GPGP',42, -152],['Australie',-25, 140],['Canada',55,-110],['Chile',-27,-66],['Chine',35,105],['Colombia', 4,-72],['Costa Rica',10,-83],['Ecuador',1,-77],['Salvador',14,-89],['Guatemala',19,-95],['Honduras',16	,-86],['Hong Kong',	23.5,	114],['Indonésie',	-1.5,	120],['Japon',33,138],['North Korea',45,124],['South korea',37,	128],['Malaisie',7,95],['Mexique',	25,	-100],['Nouvelle calédonie',-19,	165],['nicaragua'	,13,	-84],['Panama',8,-76],['Pérou'	,-10,-75],['Philiphines',13,124],['Russia',51,135],['Singapour',1,95],['USA',40,-100],['Vietnam',14,100]]
 
+for i in range (0,len(names)):
+    ax.text(names[i][2], names[i][1], names[i][0], transform=ccrs.PlateCarree(), size=5)
 
-
+#Point d'inputs des déchets dans l'océan:
+for i in range(1,GPS_points.shape[0]):
+    plt.scatter(x=GPS_points[i,2], y=GPS_points[i,3],color="black",s=1, alpha=0.5, transform= ccrs.PlateCarree())
+    
 #Mise en évidence des points pour l'affichage:
 for year in range (0,years):
     for paquet in range(0,nb_values) : #indice i du tableau 1D
@@ -64,8 +65,9 @@ for year in range (0,years):
                     if (GPGP[year,yvoisin*720+xvoisin]==0):
                         GPGP_highlight[year,yvoisin*720+xvoisin]=GPGP[year,paquet]
 
-#ANNIMATION:
+#Interval de temps pour la simmulation:
 time=np.arange(0,years,1)
+
 def init():
     pass
 
@@ -76,22 +78,21 @@ stats=0
 cbar=0
 infof=0
 
+# Fonction pour l'annimation:
 def animation_GPGP(years):
-#for i in range(months):
     global plot,date,simu,stats,cbar, infof
    
    #Remmettre tout à 0 à chaque fois:
     if years != 0:
         date.remove()
         simu.remove()
-        #stats.remove()
         cbar.remove()
         plot.remove()
        
-    # Selectionner les données différentes de 0:  
+    # Selectionner les données différentes de 0 afin de plot seulement ces dernières:  
     sel = np.where(GPGP_highlight[years,:] > 0)
         
-
+    #paramètres du graph:
     plot = plt.scatter(Longi[sel], Lati[sel], c=GPGP_highlight[years,sel], s=1, cmap=plt.cm.jet,transform=ccrs.PlateCarree())
     date = ax.annotate(f"Année:{years}\n", xy=(7, 7), xycoords='figure points', zorder=12)
     plt.title(label=f" Simulation du GPGP : The Great Pacific Garbage Patch", size= 'x-small', verticalalignment='bottom')
@@ -106,7 +107,13 @@ def animation_GPGP(years):
     simu = ax.annotate(f"Durée de la simulation: {years} ans", xy=(7, 30), xycoords='figure points', zorder=12, size='x-small')
 
     
-animGPGP = animation.FuncAnimation(fig,animation_GPGP,init_func=init, frames= time, interval=1000, repeat =False)  
-#plt.savefig('simulation_GPGP.mp4')
+animGPGP = animation.FuncAnimation(fig,animation_GPGP,init_func=init, frames= time, interval=5000, repeat =False)  
 plt.show()
 
+
+#sauvegarde de l'annimation sous forme de gif:
+'''
+f = r"/Users/adele/Desktop/animGPGP.gif" 
+writergif = animation.PillowWriter(fps=30) 
+animGPGP.save(f, writer=writergif)
+'''
